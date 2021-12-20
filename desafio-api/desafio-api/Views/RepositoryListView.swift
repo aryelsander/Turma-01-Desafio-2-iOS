00e7b4fdd74e8c517277f2ba6afb9235cee07045
Aryel Sander dos Santos <aryelsander@live.com>
2021-12-19 22:04:59 -0300

    RepositorioViewCell

--- desafio-api/desafio-api/Views/RepositoryListView.swift
+++ desafio-api/desafio-api/Views/RepositoryListView.swift
@@ -15,52 +15,7 @@ struct RepositoryListView: View {
             List(self.repositoryViewModel.repositories,id: \.id
             ){ reposVM in
                 NavigationLink(destination: PullRequestListView(repositoryName: reposVM.name, ownerName: reposVM.ownerName), label: {
-                    VStack(spacing:10){
-                        HStack(spacing: 5){
-                        Image("baixo_risco")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(height: 40)
-                            .cornerRadius(4)
-                        VStack(spacing: 5){
-                            Text("\(reposVM.ownerName)")
-                                .fontWeight(.bold)
-                                .lineLimit(1)
-                                .minimumScaleFactor(0.5)
-                            
-                            Text("\(reposVM.name)")
-                                    .fontWeight(.bold)
-                                    .lineLimit(1)
-                                    .minimumScaleFactor(0.5)
-                        }
-                            VStack(alignment: .trailing){
-                            HStack(spacing: 5){
-                                Image(systemName: "star.fill")
-                                Text("\(reposVM.startCount)")
-                                    .lineLimit(1)
-                                    .minimumScaleFactor(0.5)
-                                    .foregroundColor(.secondary)
-                            }
-                            HStack(spacing: 5){
-                                Image("fork")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(height: 20)
-                                Text("\(reposVM.forksCount)")
-                                    .lineLimit(1)
-                                    .minimumScaleFactor(0.5)
-                                    .foregroundColor(.secondary)
-                            }
-                            
-                        }
-                        
-                    }
-                   
-                        Text("\(reposVM.description ?? "")")
-                            .lineLimit(2)
-                            .minimumScaleFactor(0.6)
-                            .foregroundColor(.secondary)
-                    }
+                    RepositoryViewCell(ownerName: reposVM.ownerName, name: reposVM.name, starCount: reposVM.startCount, forksCount: reposVM.forksCount, description: reposVM.description)
                 }).buttonStyle(PlainButtonStyle())
            
                     .onAppear{
@@ -75,5 +30,61 @@ struct RepositoryListView: View {
 struct RepositoryListView_Previews: PreviewProvider {
     static var previews: some View {
         RepositoryListView()
+    }
+}
+
+struct RepositoryViewCell : View {
+    let ownerName : String
+    let name : String
+    let starCount : Int
+    let forksCount : Int
+    let description : String?
+    var body: some View {
+        VStack(spacing:10){
+            HStack(spacing: 5){
+            Image("baixo_risco")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 40)
+                .cornerRadius(4)
+            VStack(spacing: 5){
+                Text("\(ownerName)")
+                    .fontWeight(.bold)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.5)
+                
+                Text("\(name)")
+                        .fontWeight(.bold)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.5)
+            }
+                VStack(alignment: .trailing){
+                HStack(spacing: 5){
+                    Image(systemName: "star.fill")
+                    Text("\(starCount)")
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.5)
+                        .foregroundColor(.secondary)
+                }
+                HStack(spacing: 5){
+                    Image("fork")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 20)
+                    Text("\(forksCount)")
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.5)
+                        .foregroundColor(.secondary)
+                }
+                
+            }
+            
+        }
+       
+            Text("\(description ?? "")")
+                .lineLimit(2)
+                .minimumScaleFactor(0.6)
+                .foregroundColor(.secondary)
+        }
     }
 }
